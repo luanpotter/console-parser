53b70e03bc65f131a89a641757f2fc248d5a2599
Luan Nico <luanpotter28@hotmail.com>
2014-10-12 11:24:11 -0300

    Added more options for hooks on application class

--- src/main/java/xyz/luan/console/parser/Application.java
+++ src/main/java/xyz/luan/console/parser/Application.java
@@ -15,25 +15,30 @@ public abstract class Application {
     public void run(String[] args) {
         Console console = createConsole();
         Context c = createContext();
-        
+
+        executeOrLoop(args, console, c);
+        console.exit();
+    }
+
+    protected void executeOrLoop(String[] args, Console console, Context c) {
         if (args.length != 0) {
             c.execute(args);
         } else {
             console.message(startMessage());
-            
-            for (;;) {
-                String cmd = console.read();
-                CallResult r = cmd.isEmpty() ? emptyLineHandler() : c.execute(cmd);
-                if (r == CallResult.QUIT) {
-                    break;
-                } else if (r == CallResult.INVALID_COMMAND) {
-                    console.error("Invalid command. Type help for a list of valid operations.");
-                }
-            }
-
+            loop(console, c);
             console.message(endMessage());
         }
-        
-        console.exit();
+    }
+
+    protected void loop(Console console, Context c) {
+        for (;;) {
+            String cmd = console.read();
+            CallResult r = cmd.isEmpty() ? emptyLineHandler() : c.execute(cmd);
+            if (r == CallResult.QUIT) {
+                break;
+            } else if (r == CallResult.INVALID_COMMAND) {
+                console.error("Invalid command. Type help for a list of valid operations.");
+            }
+        }
     }
 }
