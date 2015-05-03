066d48a73cf00b5e52f258a324e048687c670a9c
Luan Nico <luanpotter28@hotmail.com>
2015-05-03 15:08:23 -0300

    Invalid log message displayed correctly, status code returned

--- src/main/java/xyz/luan/console/parser/Application.java
+++ src/main/java/xyz/luan/console/parser/Application.java
@@ -10,24 +10,38 @@ public abstract class Application {
     public abstract String startMessage();
     public abstract String endMessage();
 
+    protected void wrongCommandHandler(Console console) {
+        console.error("Invalid command. Type help for a list of valid operations.");
+    }
+
     public abstract CallResult emptyLineHandler();
-    
-    public void run(String[] args) {
+
+    private void handleWrongCommand(CallResult r, Console console) {
+        if (r == CallResult.INVALID_COMMAND) {
+            wrongCommandHandler(console);
+        }
+    }
+
+    public int run(String[] args) {
         Console console = createConsole();
         Context c = createContext();
 
-        executeOrLoop(args, console, c);
+        int responseCode = executeOrLoop(args, console, c);
         console.exit();
+        return responseCode;
     }
 
-    protected void executeOrLoop(String[] args, Console console, Context c) {
+    protected int executeOrLoop(String[] args, Console console, Context c) {
         if (args.length != 0) {
-            c.execute(args);
+            CallResult r = c.execute(args);
+            handleWrongCommand(r, console);
+            return r.getResponseCode();
         } else {
             console.message(startMessage());
             loop(console, c);
             console.message(endMessage());
         }
+        return CallResult.SUCCESS.getResponseCode();
     }
 
     protected void loop(Console console, Context c) {
@@ -36,9 +50,10 @@ public abstract class Application {
             CallResult r = cmd.isEmpty() ? emptyLineHandler() : c.execute(cmd);
             if (r == CallResult.QUIT) {
                 break;
-            } else if (r == CallResult.INVALID_COMMAND) {
-                console.error("Invalid command. Type help for a list of valid operations.");
+            } else {
+                handleWrongCommand(r, console);
             }
         }
     }
+
 }
--- src/main/java/xyz/luan/console/parser/call/CallResult.java
+++ src/main/java/xyz/luan/console/parser/call/CallResult.java
@@ -1,5 +1,15 @@
 package xyz.luan.console.parser.call;
 
 public enum CallResult {
-    SUCCESS, ERROR, INVALID_COMMAND, QUIT;
+    SUCCESS(0), ERROR(1), INVALID_COMMAND(2), QUIT(0);
+
+    private int responseCode;
+
+    private CallResult(int responseCode) {
+        this.responseCode = responseCode;
+    }
+
+    public int getResponseCode() {
+        return responseCode;
+    }
 }
